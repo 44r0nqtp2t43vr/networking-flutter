63f2345200872eb745eeab1857806d135d00a06c
aaronalcuitas <aaronalcuitas16@gmail.com>
2023-05-16 22:44:36 +0800

    completed networking

--- lib/helpers/album_helpers.dart
+++ lib/helpers/album_helpers.dart
@@ -23,3 +23,71 @@ Future<Album> fetchAlbum() async {
     throw Exception('Failed to load album');
   }
 }
+
+Future<Album> createAlbum(String title) async {
+  final response = await http.post(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{
+      'userId': 0,
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 201 CREATED response,
+    // then throw an exception.
+    throw Exception('Failed to create album.');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final response = await http.put(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{
+      'userId': 0,
+      'title': title,
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to update album.');
+  }
+}
+
+Future<Album> deleteAlbum(String id) async {
+  final http.Response response = await http.delete(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON. After deleting,
+    // you'll get an empty JSON `{}` response.
+    // Don't return `null`, otherwise `snapshot.hasData`
+    // will always return false on `FutureBuilder`.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a "200 OK response",
+    // then throw an exception.
+    throw Exception('Failed to delete album.');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class NetworkingApp extends StatefulWidget {
 
 class _NetworkingAppState extends State<NetworkingApp> {
   late Future<Album> futureAlbum;
+  final TextEditingController _controller = TextEditingController();
+  Future<Album>? _futureAlbum;
 
   @override
   void initState() {
@@ -37,8 +39,9 @@ class _NetworkingAppState extends State<NetworkingApp> {
         ),
         body: Column(
           children: [
-            const SizedBox(width: double.infinity),
-            Expanded(
+            SizedBox(
+              height: 80.0,
+              width: double.infinity,
               child: Center(
                 child: FutureBuilder<Album>(
                   future: futureAlbum,
@@ -47,6 +50,94 @@ class _NetworkingAppState extends State<NetworkingApp> {
                       return Text(snapshot.data!.title);
                     } else if (snapshot.hasError) {
                       return Text('${snapshot.error}');
+                    }
+
+                    // By default, show a loading spinner.
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 120.0,
+              width: double.infinity,
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(8.0),
+                child: (_futureAlbum == null)
+                    ? buildColumn()
+                    : buildFutureBuilder(),
+              ),
+            ),
+            SizedBox(
+              height: 120.0,
+              width: double.infinity,
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(8.0),
+                child: FutureBuilder<Album>(
+                  future: _futureAlbum,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            TextField(
+                              controller: _controller,
+                              decoration: const InputDecoration(
+                                hintText: 'Enter Title',
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _futureAlbum = updateAlbum(_controller.text);
+                                });
+                              },
+                              child: const Text('Update Data'),
+                            ),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+                    }
+
+                    return const CircularProgressIndicator();
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 80.0,
+              width: double.infinity,
+              child: Center(
+                child: FutureBuilder<Album>(
+                  future: _futureAlbum,
+                  builder: (context, snapshot) {
+                    // If the connection is done,
+                    // check for response data or an error.
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            ElevatedButton(
+                              child: const Text('Delete Data'),
+                              onPressed: () {
+                                setState(() {
+                                  _futureAlbum =
+                                      deleteAlbum(snapshot.data!.id.toString());
+                                  _futureAlbum = createAlbum('default');
+                                });
+                              },
+                            ),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
                     }
 
                     // By default, show a loading spinner.
@@ -78,6 +169,41 @@ class _NetworkingAppState extends State<NetworkingApp> {
           ],
         ),
       ),
+    );
+  }
+
+  Column buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          decoration: const InputDecoration(hintText: 'Enter Title'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _futureAlbum = createAlbum(_controller.text);
+            });
+          },
+          child: const Text('Create Data'),
+        ),
+      ],
+    );
+  }
+
+  FutureBuilder<Album> buildFutureBuilder() {
+    return FutureBuilder<Album>(
+      future: _futureAlbum,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data!.title);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
